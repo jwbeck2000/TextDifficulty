ec98fb1bb0767e2123ad1230cfa52c2779bef932
Kevin Borah <kevinborah@kevins-mbp.myfiosgateway.com>
2022-09-29 13:39:48 -0400

    Add feature names as additional output

--- count_vector.py
+++ count_vector.py
@@ -3,7 +3,7 @@ from sklearn.feature_extraction.text import CountVectorizer
 import pickle
 
 def count_vector(input_file_1, input_file_2, input_file_3):
-	
+
 	text_train = np.load(input_file_1, allow_pickle=True)
 	text_test = np.load(input_file_2, allow_pickle=True)
 	test_data = np.load(input_file_3, allow_pickle=True)
@@ -11,12 +11,13 @@ def count_vector(input_file_1, input_file_2, input_file_3):
 	vectorizer = CountVectorizer()
 
 	X_train = vectorizer.fit_transform(text_train)
+	X_train_feature_names = vectorizer.get_feature_names_out()
 
 	X_test = vectorizer.transform(text_test)
 
 	A_test = vectorizer.transform(test_data)
 
-	return X_train, X_test, A_test
+	return X_train, X_test, A_test, X_train_feature_names
 
 
 if __name__ == '__main__':
@@ -29,9 +30,11 @@ if __name__ == '__main__':
 	parser.add_argument('output_file_1', help='CountVector for the training data (pkl)')
 	parser.add_argument('output_file_2', help='CountVector for the testing data (pkl)')
 	parser.add_argument('output_file_3', help='TfidfVector for the actual testing data (pkl)')
+	parser.add_argument('output_file_4', help='Feature names for the training data (pkl)')
 	args = parser.parse_args()
 
 	X_train, X_test, A_test = count_vector(args.input_file_1, args.input_file_2, args.input_file_3)
 	pickle.dump(X_train, open(args.output_file_1, "wb"))
 	pickle.dump(X_test, open(args.output_file_2, "wb"))
 	pickle.dump(A_test, open(args.output_file_3, "wb"))
+	pickle.dump(X_train_feature_names, open(args.output_file_4, "wb"))
