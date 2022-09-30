f62e2f37c29d45c9b5b843e76a487c04388c7868
Kevin Borah <kevinborah@gmail.com>
2022-09-30 11:49:59 -0400

    Update KB

--- lda_topics.py
+++ lda_topics.py
@@ -17,16 +17,14 @@ def lda_init(input_file_1):
 	X_train = pickle.load(open(input_file_1, "rb"))
 
 	random_seed = 42
-
 	n_topics = 10
 
     # This will take a couple of minutes to run...
 
-    lda = LatentDirichletAllocation(n_components = n_topics, random_state=random_seed)
-    lda.fit(X_train)
+	lda = LatentDirichletAllocation(n_components = n_topics, random_state=random_seed)
+	lda.fit(X_train)
     # topic_models = lda.components_
-
-    return lda
+	return lda
 
 def display_topics(model, input_file_2, no_top_words = 8):
 
@@ -34,7 +32,7 @@ def display_topics(model, input_file_2, no_top_words = 8):
 
     topic_dict = {}
     for topic_idx, topic in enumerate(model.components_):
-        term_list = [feature_names[i]
+        term_list = [tf_feature_names[i]
                         for i in topic.argsort()[:-no_top_words - 1:-1]]
         topic_dict["topic %d:" % (topic_idx)] = term_list
 
@@ -45,15 +43,15 @@ if __name__ == '__main__':
 	import argparse
 
 	parser = argparse.ArgumentParser()
-    parser.add_argument('input_file_1', help='CountVector for the training data (pkl)')
-    parser.add_argument('input_file_2', help='Feature names of training data (pkl)')
+	parser.add_argument('input_file_1', help='CountVector for the training data (pkl)')
+	parser.add_argument('input_file_2', help='Feature names of training data (pkl)')
 
-    parser.add_argument('output_file_1', help='LDA trained model (pkl)')
+	parser.add_argument('output_file_1', help='LDA trained model (pkl)')
 	parser.add_argument('output_file_2', help='Dict of top words in n_topics (pkl)')
 	args = parser.parse_args()
 
-    model = lda_init(args.input_file_1)
-    pickle.dump(model, open(args.output_file_1, "wb"))
+	model = lda_init(args.input_file_1)
+	pickle.dump(model, open(args.output_file_1, "wb"))
 
-    topic_dict = display_topics(model, args.input_file_2)
-    pickle.dump(topic_dict, open(args.output_file_2, "wb"))
+	topic_dict = display_topics(model, args.input_file_2)
+	pickle.dump(topic_dict, open(args.output_file_2, "wb"))
