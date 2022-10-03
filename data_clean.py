80b61e3e9e5e926e206f926345b25124dc0fd9cc
Kevin Borah <kevinborah@gmail.com>
2022-10-03 13:38:15 -0400

    cleaned some Makefile-iness

--- data_clean.py
+++ data_clean.py
@@ -15,15 +15,16 @@ from tqdm import tqdm
 from sklearn.dummy import DummyClassifier
 
 def clean(input_file):
-    input_file = input_file.dropna()
+    wiki_train_df = pd.read_csv(input_file_1)
+    wiki_train_df = wiki_train_df.dropna()
     tokenized_train_items = []
     stop_words=set(stopwords.words('english'))
 
     for text in tqdm(wiki_train_df.original_text):
-        matches=re.findall(r'\w+',text)        
+        matches=re.findall(r'\w+',text)
         matches = re.findall(r"\D+",text)
         matches=[w for w in matches if not w.lower() in stop_words]
-        tokenized_train_items.append(matches) 
+        tokenized_train_items.append(matches)
     #print(tokenized_train_items[:50])
     return pd.DataFrame(tokenized_train_items)
 
@@ -35,9 +36,7 @@ if __name__ == '__main__':
 
 	parser.add_argument('output_file_1', help='Cleaned output filename')
 	args = parser.parse_args()
-	
-	wiki_train_df = pd.read_csv(input_file_1)	
-	cleaned_wiki_train_df=clean(wiki_train_df)
-	cleaned_wiki_train_df.to_csv(output_file_1,index=False) 
-    
-    
+
+	# wiki_train_df = pd.read_csv(input_file_1) Moved above - KB
+	cleaned_wiki_train_df=clean(args.input_file_1) #changed input from wiki_train_df to input_file_1
+	cleaned_wiki_train_df.to_csv(args.output_file_1,index=False) #added 'args' in front of output
