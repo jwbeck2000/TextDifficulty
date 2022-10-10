b6ae94bcee725c69d9035ae600c1fc0efd97fa1c
Kevin Borah <kevinborah@gmail.com>
2022-10-10 11:17:41 -0500

    Commit lda_gensim model

--- lda_gensim.py
+++ lda_gensim.py
@@ -48,3 +48,20 @@ def lda_gensim(input_file_1): #take in combined_cleaned.csv and make list of lis
     num_topics = 10
     lda_model = gensim.models.LdaMulticore(corpus = corpus,id2word = id2word, num_topics = num_topics)
     doc_lda = lda_model[corpus]
+
+    import pyLDAvis.gensim_models
+
+    pyLDAvis.enable_notebook()
+
+    LDAvis_data_filepath = '/work/reults/ldavis_prepared_10.pickle'
+    # # this is a bit time consuming - make the if statement True
+    # # if you want to execute visualization prep yourself
+    if 1 == 1:
+        LDAvis_prepared = pyLDAvis.gensim_models.prepare(lda_model, corpus, id2word)
+        with open(LDAvis_data_filepath, 'wb') as f:
+            pickle.dump(LDAvis_prepared, f)
+    # load the pre-prepared pyLDAvis data from disk
+    with open(LDAvis_data_filepath, 'rb') as f:
+        LDAvis_prepared = pickle.load(f)
+    pyLDAvis.save_html(LDAvis_prepared, '/work/reults/ldavis_prepared_10.html')
+    LDAvis_prepared
